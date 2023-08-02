465bb1c6426e0bff6e6a539bbf7d311599a39399
tangilibrt <tanguy.libert@efrei.net>
2023-08-02 16:23:35 +0200

    First Commit :  - Home page web version --> up to the beginning of the reservation part

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeaderVue/>
+  <main>
+    <router-view />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderVue from "@/components/Header/HeaderVue.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderVue,
   }
 }
 </script>
 
 <style>
+
+*, *::before, *::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Cinzel, serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: black;
+  min-height: 1000px;
+  overflow-x: hidden;
 }
+
+@import url('https://fonts.googleapis.com/css2?family=Inter&display=swap');
+
+@font-face {
+  font-family: Cinzel;
+  src: url(@/assets/Cinzel-VariableFont_wght.ttf);
+}
+
+
 </style>
